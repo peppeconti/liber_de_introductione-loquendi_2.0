be2fa64ef35da09f4ac726130f1d33e5a4eaec7a
peppeconti <46853326+peppeconti@users.noreply.github.com>
2024-09-25 22:21:32 +0200

    update

--- src/app/datas/datas.component.ts
+++ src/app/datas/datas.component.ts
@@ -3,7 +3,10 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { catchError, map, throwError } from "rxjs";
 import { LatinComponent } from "../latin/latin.component";
 
-const headers = new HttpHeaders({ "Content-Type": "text/mxl" }).set("Accept", "text/xml");
+const headers = new HttpHeaders({ "Content-Type": "text/mxl" }).set(
+  "Accept",
+  "text/xml"
+);
 
 @Component({
   selector: "app-datas",
@@ -19,27 +22,27 @@ export class DatasComponent implements OnInit {
 
   ngOnInit() {
     const subscription = this.httpClient
-      .get("assets/data/liber_de_introductione_loquendi.xml", { 
+      .get("assets/data/liber_de_introductione_loquendi.xml", {
         headers: headers,
-        responseType: "text"
-       })
+        responseType: "text",
+      })
       //.pipe(map((res) => this.parseXML(this.minifyXml(res))))
       .pipe(map((res) => this.parseXML(res)))
       .subscribe({
         next: (resData) => {
           this.data.set(resData);
-          //console.log(this.data)
+          if (this.data) console.log(this.createPages(this.data()!));
         },
         error: (error) => console.log(error.message),
         complete: () => {
-          console.log("completed")
+          console.log("completed");
         },
       });
 
     this.destroyRef.onDestroy(() => subscription.unsubscribe());
   }
 
-  minifyXml(xml: string): string {
+  /*minifyXml(xml: string): string {
     let minified: string;
     minified = xml
       .split(/(?<=>)\s*(?=<)/)
@@ -47,12 +50,22 @@ export class DatasComponent implements OnInit {
       .split(/\n/)
       .join("");
     return minified;
-  }
+  }*/
 
   parseXML(res: string): Document {
     const parser: DOMParser = new DOMParser();
     const xml: Document = parser.parseFromString(res, "application/xml");
     return xml;
   }
-  
+
+  createPages(input: Document) {
+    const pageBreakMarkers: NodeList = input?.querySelectorAll("pb");
+    const ff = Array.from(pageBreakMarkers).map((pb) => {
+        return (<Element>pb).nextElementSibling;
+    });
+    /*const ff = Array.from(pageBreakMarkers).map(pb => {
+      return (<Element>pb).nodeName}
+    );*/
+    console.log(ff);
+  }
 }
