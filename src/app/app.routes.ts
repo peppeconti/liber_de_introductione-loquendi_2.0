5e1a9166adbf2191b9aefee5852daf5a44e9703b
peppeconti <46853326+peppeconti@users.noreply.github.com>
2024-09-30 01:13:46 +0200

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,28 +1,13 @@
 import { Routes } from '@angular/router';
-import { NoTaskComponent } from './tasks/no-task/no-task.component';
-import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
-import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   {
-    path: '',
-    component: NoTaskComponent,
+    path: 'Home',
+    component: Home ,
   },
   {
-    path: 'users/:uid',
-    component: UserTasksComponent,
-    children: [
-      {
-        path: 'tasks',
-        component: TasksComponent // needs a separate router outlet
-      },
-      {
-        path: 'tasks/new',
-        component: NewTaskComponent // needs a separate router outlet
-      },
-    ]
+    path: ':folio',
+    component:  ,
   },
   {
     path: '**',
