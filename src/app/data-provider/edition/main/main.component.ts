abdf9a103bcea2c7a85d5835b28da8b85a1eb61d
peppeconti <46853326+peppeconti@users.noreply.github.com>
2024-09-30 21:51:26 +0200

    update

--- src/app/data-provider/edition/main/main.component.ts
+++ src/app/data-provider/edition/main/main.component.ts
@@ -1,27 +1,37 @@
-import { Component, computed, inject, OnInit, input } from '@angular/core';
-import { JsonNode } from '../../../services/models';
-import { HttpService } from '../../../services/httpService.service';
+import { Component, computed, inject, OnInit, input } from "@angular/core";
+import { JsonNode } from "../../../services/models";
+import { HttpService } from "../../../services/httpService.service";
 import { LatinTextComponent } from "./latin-text/latin-text.component";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-main',
+  selector: "app-main",
   standalone: true,
   imports: [LatinTextComponent],
-  templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+  templateUrl: "./main.component.html",
+  styleUrl: "./main.component.css",
 })
 export class MainComponent implements OnInit {
+  private router = inject(Router);
   private httpService = inject(HttpService);
-  latin_text = computed<JsonNode[]>(() => this.getLatinText());
-  folio = input.required<string>()
+  latin_text = computed<JsonNode[] | undefined | null>(() =>
+    this.getLatinText()
+  );
+  folio = input.required<string>();
 
   ngOnInit(): void {
-    console.log(this.latin_text());
+    // console.log(this.latin_text());
   }
 
   private getLatinText() {
-    const latin_document: Element = this.httpService.data()?.getElementById(this.folio())!;
-    const latin_json = this.httpService.parseNode(latin_document);
-    return [latin_json];
+    const latin_document: HTMLElement | null | undefined = this.httpService
+      .data()!
+      .getElementById(this.folio());
+    if (latin_document) {
+      const latin_json = this.httpService.parseNode(<Element>latin_document);
+      return [latin_json];
+    } else {
+      return undefined;
+    }
   }
 }
