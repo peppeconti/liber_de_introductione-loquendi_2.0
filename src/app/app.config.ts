a7b1e7e1b1673535c3f9262257f538886c6a2699
peppeconti <46853326+peppeconti@users.noreply.github.com>
2024-09-30 17:13:57 +0200

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,21 @@
-import { ApplicationConfig } from "@angular/core"
-import { provideRouter, withComponentInputBinding, withRouterConfig } from "@angular/router"
-import { routes } from "./app.routes"
+import { ApplicationConfig } from "@angular/core";
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withRouterConfig,
+} from "@angular/router";
+import { routes } from "./app.routes";
+import { provideHttpClient } from "@angular/common/http";
 
 export const AppConfig: ApplicationConfig = {
-    providers: [
-        provideRouter(routes, withComponentInputBinding(), withRouterConfig({
-            paramsInheritanceStrategy: 'always'
-        }))
-    ]
-}
+  providers: [
+    provideHttpClient(),
+    provideRouter(
+      routes,
+      withComponentInputBinding(),
+      withRouterConfig({
+        paramsInheritanceStrategy: "always",
+      })
+    ),
+  ],
+};
