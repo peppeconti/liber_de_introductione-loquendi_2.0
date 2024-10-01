e52d2ae9b86ee58bc87c64cb3f39436e940b57f5
peppeconti <46853326+peppeconti@users.noreply.github.com>
2024-10-01 10:02:37 +0200

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,6 @@ import { NotFoundComponent } from './data-provider/not-found/not-found.component
 
 export const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'credits',
-    pathMatch: 'full',
-  },
-  {
     path: 'credits',
     component: CreditsComponent
   },
