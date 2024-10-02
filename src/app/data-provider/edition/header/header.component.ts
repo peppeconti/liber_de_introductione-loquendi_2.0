7085d657a05c26305d1451f710a773aa1abf0137
peppeconti <46853326+peppeconti@users.noreply.github.com>
2024-10-02 17:44:53 +0200

    update

--- src/app/data-provider/edition/header/header.component.ts
+++ src/app/data-provider/edition/header/header.component.ts
@@ -2,11 +2,12 @@ import { Component, computed, input } from "@angular/core";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { faGear, faGears, faHouse } from "@fortawesome/free-solid-svg-icons";
 import { DropdownComponent } from "./dropdown/dropdown.component";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: "app-header",
   standalone: true,
-  imports: [FontAwesomeModule, DropdownComponent],
+  imports: [FontAwesomeModule, DropdownComponent, RouterLink],
   templateUrl: "./header.component.html",
   styleUrl: "./header.component.css"
 })
---
+++ src/app/data-provider/edition/main/note-container/note-text/note-text.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-note-text',
+  standalone: true,
+  imports: [],
+  templateUrl: './note-text.component.html',
+  styleUrl: './note-text.component.css'
+})
+export class NoteTextComponent {
+
+}
