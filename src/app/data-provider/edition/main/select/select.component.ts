0d8c450016f5e8e33b3de1589383bfadddabd2a4
peppeconti <46853326+peppeconti@users.noreply.github.com>
2024-10-03 00:08:51 +0200

    update

--- src/app/data-provider/edition/main/select/select.component.ts
+++ src/app/data-provider/edition/main/select/select.component.ts
@@ -1,14 +1,42 @@
-import { Component, input } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, ElementRef, input, OnChanges, SimpleChanges, ViewChild } from "@angular/core";
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
-  selector: 'app-select',
+  selector: "app-select",
   standalone: true,
   imports: [RouterLink, RouterLinkActive],
-  templateUrl: './select.component.html',
-  styleUrl: './select.component.css'
+  templateUrl: "./select.component.html",
+  styleUrl: "./select.component.css",
 })
-export class SelectComponent {
+export class SelectComponent implements OnChanges {
+  
   folios = input.required<(string | null)[]>();
   folio = input.required<string>();
+  @ViewChild("el") ul: ElementRef | undefined;
+
+  ngAfterViewInit() {
+    const scrollDiv = Array.from(this.ul?.nativeElement.children);
+    const selected = scrollDiv.find(
+      (e) => (<Element>e).getAttribute("id") === this.folio()
+    );
+    //console.log((<HTMLElement>selected).offsetTop);
+    (<HTMLElement>selected).scrollIntoView();
+    //this.ul?.nativeElement.scrollTo({ top: 50, behavior: "smooth" });
+    console.log(Array.from(this.ul?.nativeElement.children));
+    //console.log('ciap')
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    //throw new Error("Method not implemented.");
+    console.log('ciap')
+    if(this.ul?.nativeElement) {
+      const scrollDiv = Array.from(this.ul!.nativeElement.children);
+      const selected = scrollDiv.find(
+        (e) => (<Element>e).getAttribute("id") === this.folio()
+      );
+      console.log((<HTMLElement>selected).getAttribute('id'))
+      console.log((<HTMLElement>selected));
+      console.log(this.ul?.nativeElement.scrollTop);
+    }
+  }
 }
