9d6385be818d8740f13ba25f8b78acae49313f9d
peppeconti <46853326+peppeconti@users.noreply.github.com>
2024-10-04 22:24:13 +0200

    update

--- src/app/data-provider/home/home.component.ts
+++ src/app/data-provider/home/home.component.ts
@@ -1,13 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { HomeHeaderComponent } from "./home-header/home-header.component";
 import { HomeMainComponent } from "./home-main/home-main.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HomeHeaderComponent, HomeMainComponent],
+  imports: [HomeHeaderComponent, HomeMainComponent, RouterLink],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  //encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent {
   @Input({required: true}) data: Document | undefined;
