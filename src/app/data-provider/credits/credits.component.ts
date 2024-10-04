a0fa5efe4947fde8b3cc9f7a83fffc677d24a645
peppeconti <46853326+peppeconti@users.noreply.github.com>
2024-10-04 13:49:18 +0200

    update

--- src/app/data-provider/credits/credits.component.ts
+++ src/app/data-provider/credits/credits.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { HomeHeaderComponent } from "./home-header/home-header.component";
 
 @Component({
   selector: 'app-credits',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, HomeHeaderComponent],
   templateUrl: './credits.component.html',
   styleUrl: './credits.component.css'
 })
