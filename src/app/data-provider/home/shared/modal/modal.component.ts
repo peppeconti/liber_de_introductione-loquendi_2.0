aef54fb684356e565d4fe0d908f2b0ccb733be84
peppeconti <46853326+peppeconti@users.noreply.github.com>
2024-10-05 14:02:45 +0200

    update

--- src/app/data-provider/home/shared/modal/modal.component.ts
+++ src/app/data-provider/home/shared/modal/modal.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-modal',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.css'
 })
