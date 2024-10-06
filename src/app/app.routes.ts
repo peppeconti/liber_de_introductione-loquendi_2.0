ba7334f0ae3bc998d2a4d3ed48e2f665103ab676
peppeconti <46853326+peppeconti@users.noreply.github.com>
2024-10-06 15:23:13 +0200

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './data-provider/home/home.component';
 import { EditionComponent, resolveTitle} from './data-provider/edition/edition.component';
 import { NotFoundComponent } from './data-provider/not-found/not-found.component';
-import { BiblioComponent } from './data-provider/home/biblio/biblio.component';
-import { CodexComponent } from './data-provider/home/codex/codex.component';
-import { CreditsComponent } from './data-provider/home/credits/credits.component';
+import { childrenRoutes } from './data-provider/home/children.routes';
 
 export const routes: Routes = [
   {
@@ -16,23 +14,7 @@ export const routes: Routes = [
     path: 'home',
     component: HomeComponent,
     title: 'LIL - Home',
-    children: [
-      {
-        path: 'codex',
-        component: CodexComponent,
-        title: 'LIL - Codex',
-      },
-      {
-        path: 'bibliography',
-        component: BiblioComponent,
-        title: 'LIL - Bibliography',
-      },
-      {
-        path: 'credits',
-        component: CreditsComponent,
-        title: 'LIL - Credits',
-      },
-    ]
+    children: childrenRoutes
   },
   {
     path: 'edition/:folio',
