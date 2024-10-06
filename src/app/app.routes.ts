427d9cf044c33357a72dc2c1ae26cbaca4300937
peppeconti <46853326+peppeconti@users.noreply.github.com>
2024-10-06 13:09:27 +0200

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './data-provider/home/home.component';
-import { EditionComponent } from './data-provider/edition/edition.component';
+import { EditionComponent, resolveTitle} from './data-provider/edition/edition.component';
 import { NotFoundComponent } from './data-provider/not-found/not-found.component';
 import { BiblioComponent } from './data-provider/home/biblio/biblio.component';
 import { CodexComponent } from './data-provider/home/codex/codex.component';
@@ -15,27 +15,33 @@ export const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+    title: 'LIL - Home',
     children: [
       {
         path: 'codex',
         component: CodexComponent,
+        title: 'LIL - Codex',
       },
       {
-        path: 'bibliografy',
+        path: 'bibliography',
         component: BiblioComponent,
+        title: 'LIL - Bibliography',
       },
       {
         path: 'credits',
         component: CreditsComponent,
+        title: 'LIL - Credits',
       },
     ]
   },
   {
     path: 'edition/:folio',
-    component: EditionComponent
+    component: EditionComponent,
+    title: resolveTitle,
   },
   {
     path: '**',
-    component: NotFoundComponent
+    component: NotFoundComponent,
+    title: 'Error 404',
   }
 ];
--- src/app/data-provider/edition/edition.component.ts
+++ src/app/data-provider/edition/edition.component.ts
@@ -2,6 +2,11 @@ import { Component, input, Input, computed } from "@angular/core";
 import { HeaderComponent } from "./header/header.component";
 import { MainComponent } from "./main/main.component";
 import { NotFoundComponent } from "../not-found/not-found.component";
+import {
+  ActivatedRouteSnapshot,
+  ResolveFn,
+  RouterState,
+} from "@angular/router";
 
 @Component({
   selector: "app-edition",
@@ -11,10 +16,23 @@ import { NotFoundComponent } from "../not-found/not-found.component";
   styleUrl: "./edition.component.css",
 })
 export class EditionComponent {
-  @Input({required: true}) data: Document | undefined;
+  @Input({ required: true }) data: Document | undefined;
   folio = input.required<string>();
   isExistingFolio = computed(() => {
-    const folio: HTMLElement | null | undefined = this.data?.getElementById(this.folio());
+    const folio: HTMLElement | null | undefined = this.data?.getElementById(
+      this.folio()
+    );
     return folio ? true : false;
-  })
+  });
+
 }
+
+/*export const resolveTitle: ResolveFn<string> = (
+  activatedRoute: ActivatedRouteSnapshot,
+  routerState: RouterState
+) => activatedRoute.paramMap.get("folio")?.replace("_", "");*/
+
+export const resolveTitle: ResolveFn<string> = (
+  activatedRoute: ActivatedRouteSnapshot,
+  routerState: RouterState
+) => 'Edition - ' + activatedRoute.paramMap.get('folio');
