1302a170f353b52e88bf454d7834e9588882b7a5
peppeconti <46853326+peppeconti@users.noreply.github.com>
2024-10-12 21:05:04 +0200

    update

--- src/app/data-provider/home/biblio/secondary-biblio/secondary-biblio.component.ts
+++ src/app/data-provider/home/biblio/secondary-biblio/secondary-biblio.component.ts
@@ -1,23 +1,41 @@
 import { Component, computed, Input, OnInit } from "@angular/core";
 import { JsonNode } from "../../../../services/models";
-import { NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault, NgTemplateOutlet } from "@angular/common";
+import {
+  NgFor,
+  NgIf,
+  NgSwitch,
+  NgSwitchCase,
+  NgSwitchDefault,
+  NgTemplateOutlet,
+} from "@angular/common";
 import { isSubset } from "../../../../utils/utils";
 
 @Component({
   selector: "app-secondary-biblio",
   standalone: true,
-  imports: [NgSwitch, NgSwitchCase, NgSwitchDefault, NgFor, NgIf, NgTemplateOutlet],
+  imports: [
+    NgSwitch,
+    NgSwitchCase,
+    NgSwitchDefault,
+    NgFor,
+    NgIf,
+    NgTemplateOutlet,
+  ],
   templateUrl: "./secondary-biblio.component.html",
   styleUrl: "./secondary-biblio.component.css",
 })
 export class secondaryBiblioComponent implements OnInit {
   @Input({ required: true }) secondary_biblio: JsonNode[] | undefined | null;
-  monographies = computed(() => this.secondary_biblio?.filter(e => e.attributes![0].value === 'monography'));
-  articles = computed(() => this.secondary_biblio?.filter(e => e.attributes![0].value === 'article'))
+  monographies = computed(() =>
+    this.secondary_biblio?.filter(
+      (e) => e.attributes![0].value === "monography"
+    )
+  );
+  articles = computed(() =>
+    this.secondary_biblio?.filter((e) => e.attributes![0].value === "article")
+  );
 
   isSubset = isSubset;
 
-  ngOnInit(): void {
-   
-  }
+  ngOnInit(): void {}
 }
