673982dd9b1e610f6ae7057b6c6ef2d273a3ec23
peppeconti <46853326+peppeconti@users.noreply.github.com>
2024-10-16 13:16:16 +0200

    update

--- src/app/data-provider/home/credits/credits.component.ts
+++ src/app/data-provider/home/credits/credits.component.ts
@@ -4,12 +4,11 @@ import { findAttributeValue, isSubset } from "../../../utils/utils";
 import { NgFor, NgSwitch, NgSwitchCase, NgSwitchDefault, NgTemplateOutlet } from "@angular/common";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { faDownload, faLink } from "@fortawesome/free-solid-svg-icons";
-import { ListNameDirective } from "../../../directives/list-name.directive";
 
 @Component({
   selector: "app-credits",
   standalone: true,
-  imports: [NgFor, NgSwitch, NgSwitchCase, NgSwitchDefault, NgTemplateOutlet, FontAwesomeModule, ListNameDirective],
+  imports: [NgFor, NgSwitch, NgSwitchCase, NgSwitchDefault, NgTemplateOutlet, FontAwesomeModule],
   templateUrl: "./credits.component.html",
   styleUrl: "./credits.component.css",
 })
