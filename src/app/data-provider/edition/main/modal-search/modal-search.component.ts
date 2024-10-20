709f7e1df538a570b996eca59c173cd5872ab4dd
peppeconti <46853326+peppeconti@users.noreply.github.com>
2024-10-20 21:56:59 +0200

    update

--- src/app/data-provider/edition/main/modal-search/modal-search.component.ts
+++ src/app/data-provider/edition/main/modal-search/modal-search.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { SearchComponent } from "../../shared/search/search.component";
+import { ModalDirective } from '../../../../directives/modal.directive';
+
+declare const bootstrap: any;
 
 @Component({
   selector: 'app-modal-search',
   standalone: true,
-  imports: [SearchComponent],
+  imports: [SearchComponent, ModalDirective],
   templateUrl: './modal-search.component.html',
   styleUrl: './modal-search.component.css'
 })
 export class ModalSearchComponent {
 
+  modal = signal<any | undefined>(undefined);
+
+  ngOnInit() {
+    const modal = new bootstrap.Modal("#modal-search");
+    this.modal.set(modal);
+    console.log(this.modal());
+  }
+
+  onHide() {
+    window.location.hash = "";
+  }
+
+  onShow() {
+    window.location.hash = "search";
+  }
+
+  ngOnDestroy(): void {
+    console.log("modal-search destroyed!")
+    this.modal().dispose();
+  }
 }
