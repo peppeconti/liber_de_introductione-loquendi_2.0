ee281eb625fbbc4b6dd73ca27cd6a1e6bd933af8
peppeconti <46853326+peppeconti@users.noreply.github.com>
2026-09-02 11:17:51 +0200

    Remove unused @angular/localize dependency

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { provideZoneChangeDetection } from "@angular/core";
-/// <reference types="@angular/localize" />
 
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { AppConfig } from './app/app.config';
 
 
-bootstrapApplication(AppComponent, {...AppConfig, providers: [provideZoneChangeDetection(), ...AppConfig.providers]}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, { ...AppConfig, providers: [provideZoneChangeDetection(), ...AppConfig.providers] }).catch((err) => console.error(err));
